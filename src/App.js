e34314c4ca1bc79cc3d60b7b3823dc58c7dba5f2
Juan Carlos Orcko <juancarlosorcko78@gmail.com>
2022-11-07 23:29:52 -0400

    Storybook implementado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Modal from './components/Modal';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Prueba el modal.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>Mostrar modal</button>
+        <Modal></Modal>
       </header>
+      
     </div>
   );
 }
